3ec70334d95c716d8ac7761148ba414163743022
Ogheneyoma <Lawrenceyoma@gmail.com>
2025-04-03 19:16:28 +0100

    feat: added the real api link

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,8 @@
 // @ts-nocheck
 
 // const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001/api";
-const API_URL = "http://localhost:3001/api";
+// const API_URL = "http://localhost:3001/api";
+const API_URL = "https://zora-be.onrender.com/api";
 
 // Event API calls
 export async function getEvents() {
